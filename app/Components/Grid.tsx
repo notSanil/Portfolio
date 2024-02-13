12c22cd034350deb6433fe3fb3b8bead0bb5fca8
notSanil <44319106+notSanil@users.noreply.github.com>
2024-02-14 00:26:17 +0530

    Add stagger grid effect on click

--- app/Components/Grid.tsx
+++ app/Components/Grid.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { stagger, useAnimate, motion } from "framer-motion";
 import Tile from "./GridTile";
 import { useState, useEffect } from "react";
 
@@ -6,22 +7,57 @@ interface GridProps {
     callback: () => void
 }
 
-export default function Grid({callback}: GridProps) {
+export default function Grid({ callback }: GridProps) {
     const [windowDimensions, setWindowDimensions] = useState({ width: 1080, height: 720 });
     const [visible, setVisible] = useState(true);
+    const [key, setKey] = useState(0);
+
+    const [scope, animate] = useAnimate();
+
+    // useEffect(() => {
+    //     const handleResize = () => {
+    //         setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
+    //     };
+    //     handleResize();
+
+    //     window.addEventListener('resize', handleResize);
+
+
+    //     return () => {
+    //         window.removeEventListener('resize', handleResize);
+    //     };
+    // }, []);
 
     useEffect(() => {
         const handleResize = () => {
             setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
         };
         handleResize();
-
         window.addEventListener('resize', handleResize);
+
+        const tileRefs = document.querySelectorAll(".tile");
+        console.log(tileRefs);
+
+        animate(
+            ".tile",
+            visible
+                ? { opacity: 1 }
+                : { opacity: 0 },
+            {
+                duration: 1,
+                delay: stagger(
+                    0.01,
+                    {
+                        from: key,
+                    }
+                )
+            }
+        );
 
         return () => {
             window.removeEventListener('resize', handleResize);
         };
-    }, []);
+    }, [visible]);
 
     const numRows = Math.floor(windowDimensions.width / 100);
     const numCols = Math.floor(windowDimensions.height / 100);
@@ -30,12 +66,11 @@ export default function Grid({callback}: GridProps) {
         return (
             <Tile
                 key={key}
-                callback={()=>{
-                    console.log(key);
+                callback={() => {
                     setVisible(!visible);
+                    setKey(key);
                     callback();
                 }}
-                visible={visible}
             />
         );
     };
@@ -53,6 +88,7 @@ export default function Grid({callback}: GridProps) {
     return (
         <div
             style={{ display: 'grid', gridTemplateColumns: `repeat(${numRows}, 1fr)`, position: 'relative', gridTemplateRows: `repeat(${numCols}, 1fr)`, height: `calc(100vh - 1px)`, width: `calc(100vw - 1px)`, zIndex: 2 }}
+            ref={scope}
         >
             {renderGrid()}
         </div>
--- app/Components/GridTile.tsx
+++ app/Components/GridTile.tsx
@@ -1,21 +1,21 @@
 interface TileProps {
     callback: () => void,
-    visible: boolean
 }
 
 
-export default function Tile({ callback, visible }: TileProps) {
+function Tile({ callback }: TileProps) {
     const marginSize: number = 0.7;
 
     return (
         <div
             style={{
                 margin: 0.5,
-                opacity: visible ? 1 : 0
             }}
-            className={`hover:bg-[#1e1e1e] bg-[#0f0f0f]`}
+            className={`hover:bg-[#1e1e1e] bg-[#0f0f0f] tile`}
             onClick={() => callback()}
         >
         </div>
     );
 }
+
+export default Tile;
