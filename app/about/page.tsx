4889bffad0c8bf271a6dcd2f815c04bace6fe290
notSanil <44319106+notSanil@users.noreply.github.com>
2024-03-05 19:05:55 +0530

    Made about page responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,41 +8,44 @@ export default function AboutMe() {
   return (
     <main className="flex flex-col min-h-screen">
       <Navbar page={Pages.About} />
-      <div className="bg-primary p-4 text-center flex-1 text-zinc-100">
-        <h1 className="text-center w-full text-[3.5vw]">
+      <div className="bg-primary p-2 text-center flex-1 text-zinc-100">
+        <h1 className="m-2 text-center w-full text-2xl md:text-[3.5vw]">
           A little bit about me
         </h1>
         <Image
           src={profilePic}
           alt="Sanil"
-          className="self-center rounded-full left-1/2 relative -translate-x-1/2 w-40 h-40"
+          className="rounded-full mx-auto size-32 md:size-40"
         />
-        <p className="relative text-center w-1/2 left-1/4 my-4 text-[1.4vw]">
-          Hi! My name is Sanil, and I am a software developer with a passion for
-          back-end development, & graphics. I am currently a fourth-year student
-          at{" "}
-          <Link
-            href="https://pec.ac.in/"
-            target="_blank"
-            className="text-blue-chill-400"
-          >
-            Punjab Engineering College{" "}
-          </Link>
-          pursuing a degree in{" "}
-          <Link
-            href="https://pec.ac.in/cse"
-            target="_blank"
-            className="text-blue-chill-400"
-          >
-            Computer Science.
-          </Link>{" "}
-          I aspire towards a career that will allow me to create impact while
-          working on challenging problems.
-          <br></br>
-          <br></br>
-          When I am not coding, I enjoy video games, petting dogs, and cycling.
-        </p>
-        <SelfProclamations />
+        <div className="mx-4 md:w-1/2 md:m-auto md:p-1">
+          <p className="relative text-center my-4 text-base md:text-[1.4vw]">
+            Hi! My name is Sanil, and I am a software developer with a passion
+            for back-end development, & graphics. I am currently a fourth-year
+            student at{" "}
+            <Link
+              href="https://pec.ac.in/"
+              target="_blank"
+              className="text-blue-chill-400"
+            >
+              Punjab Engineering College{" "}
+            </Link>
+            pursuing a degree in{" "}
+            <Link
+              href="https://pec.ac.in/cse"
+              target="_blank"
+              className="text-blue-chill-400"
+            >
+              Computer Science.
+            </Link>{" "}
+            I aspire towards a career that will allow me to create impact while
+            working on challenging problems.
+            <br></br>
+            <br></br>
+            When I am not coding, I enjoy video games, petting dogs, and
+            cycling.
+          </p>
+          <SelfProclamations />
+        </div>
       </div>
     </main>
   );
@@ -50,28 +53,28 @@ export default function AboutMe() {
 
 function SelfProclamations() {
   return (
-    <div className="text-center bg-blue-chill-950 bg-opacity-20 w-1/2 left-1/4 relative mb-4 rounded-sm text-zinc-100 border-blue-chill-600 border">
-      <h1 className="text-[1.875vw]">TL;DR? Self Proclamations</h1>
+    <div className="text-center bg-blue-chill-950 relative mb-4 rounded-sm text-zinc-100 border-blue-chill-600 border p-2">
+      <h1 className="text-base md:text-[1.875vw]">TL;DR? Self Proclamations</h1>
       <div className="flex flex-row relative justify-between">
-        <div className="m-2 grow basis-0 overflow-hidden">
+        <div className="m-2 grow basis-0 overflow-hidden text-[10px] md:text-base">
           <div>
-            <span className="material-symbols-outlined text-[4.688vw]">
+            <span className="material-symbols-outlined text-5xl md:text-[4.688vw]">
               keyboard
             </span>
           </div>
           Software developer
         </div>
-        <div className="m-2">
+        <div className="m-2 text-[10px] md:text-base">
           <div>
-            <span className="material-symbols-outlined text-[4.688vw]">
+            <span className="material-symbols-outlined text-5xl md:text-[4.688vw]">
               stadia_controller
             </span>
           </div>
           Gamer
         </div>
-        <div className="m-2 grow basis-0 overflow-hidden">
+        <div className="m-2 grow basis-0 overflow-hidden text-[10px] md:text-base">
           <div>
-            <span className="material-symbols-outlined text-[4.688vw]">
+            <span className="material-symbols-outlined text-5xl md:text-[4.688vw]">
               directions_bike
             </span>
           </div>
